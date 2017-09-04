adec24691c08265be62fdc9bdf1495b36f9a378f
fctest <1379079823@qq.com>
2017-09-04 18:29:51 +0800

    work commit

--- springboot-webModule/src/main/java/com/boot/mybatis/util/PluginIntercept.java
+++ springboot-webModule/src/main/java/com/boot/mybatis/util/PluginIntercept.java
@@ -1,15 +1,19 @@
 package com.boot.mybatis.util;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.springframework.util.ReflectionUtils;
 
 @Intercepts(@Signature(args= {Connection.class,Integer.class},method= "prepare",type= StatementHandler.class))
 public class PluginIntercept implements Interceptor{
@@ -22,6 +26,10 @@ public class PluginIntercept implements Interceptor{
 		BoundSql boundSql=handler.getBoundSql();
 		Object obj=boundSql.getParameterObject();
 		String sql=boundSql.getSql();
+		List<ParameterMapping> list=boundSql.getParameterMappings();
+//		Configuration configuration= (Configuration) reflectField("configuration",list.get(0));      //反射获取configuration
+//		BoundSql pageBound=new BoundSql(configuration,generatePageSql(sql),boundSql.getParameterMappings(),obj);   //重新组建修改过的包含分页语句的sql
+		setField("sql", boundSql,generatePageSql(sql));                //修改Bound中的sql值
 		return invocation.proceed();
 	}
 
@@ -40,6 +48,25 @@ public class PluginIntercept implements Interceptor{
 	public void setProperties(Properties properties) {
 		// TODO Auto-generated method stub
 		
+	}
+	public String generatePageSql(String sql){
+		StringBuilder pageSql=new StringBuilder();
+		pageSql.append("select tmp.*,rownum from(").append(sql).append(")tmp where rownum<=10");
+		return pageSql.toString();
+	}
+	public <T> Object reflectField(String fieldName,T obj){
+		Field field=ReflectionUtils.findField(obj.getClass(),fieldName);
+		field.setAccessible(true);
+		return ReflectionUtils.getField(field, obj);
+	}
+	public <T> void setField(String fieldName,BoundSql boundSql,String string){
+		Field field=ReflectionUtils.findField(boundSql.getClass(),fieldName);
+		field.setAccessible(true);
+		try {
+			field.set(boundSql,string);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
